606d66b01a917d9154943c254ccc4fd94649275c
Silvina Rodriguez <silvinaalerodriguez@gmail.com>
2022-10-20 22:16:59 -0300

    Hay ejercicios a los que les puse # y no hay que tenrmos en cuenta porque lo hice para practicar

--- mi_primer_mvt/mi_mvt/mi_mvt/urls.py
+++ mi_primer_mvt/mi_mvt/mi_mvt/urls.py
@@ -24,12 +24,12 @@ from familia.views import template_loader
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', views.hello_world),
-    path('title/', views.title),
-    path('her-name-is/<str:nombre>/<int:edad>/', views.her_name_is),
+    path('hello-world/', views.hello_world), #practicas, no tener en cuenta
+    path('title/', views.title), #practicas, no tener en cuenta
+    path('her-name-is/<str:nombre>/<int:edad>/', views.her_name_is), #practicas, no tener en cuenta
     path('my-template/<str:nombre>/<str:apellido>/<str:relacion>/<str:nacimiento>/<int:edad>/', views.my_template),
     path('count/', views.count),
-    path('template-loader/<str:nombre>/<str:apellido>/<str:relacion>/<str:nacimiento>/<int:edad>/', template_loader), 
+    path('template-loader/<str:nombre>/<str:apellido>/<str:relacion>/<str:nacimiento>/<int:edad>/', template_loader), #practicas, no tener en cuenta 
     path("familia/", include("familia.urls")) 
 
    
